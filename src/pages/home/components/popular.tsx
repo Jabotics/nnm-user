7c98eef9b88eae1bd6023c98aa117ebf8f0edb88
ciekotics <rahul.saxena@jabotics.in>
2024-06-03 23:44:31 +0530

    popular products

--- src/pages/home/components/popular.tsx
+++ src/pages/home/components/popular.tsx
@@ -2,7 +2,7 @@ const Popular = () => {
   return (
     <div className='mt-20 w-screen'>
       <div className='flex w-[93vw] items-center justify-between'>
-        <h1 className='text-[1.75rem] font-medium'>Featured Categories</h1>
+        <h1 className='text-[1.75rem] font-medium'>Popular Products</h1>
         <div className='mt-2 mr-7 flex items-center gap-8 text-sm'>
           {[
             { title: 'Baby & Mom Care' },
